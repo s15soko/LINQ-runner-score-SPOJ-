845cff99f30229c3e20d984e879a149f9b6f5b68
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-22 20:30:37 +0100

    Add program initial files

---
+++ LINQ_runner_scores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_runner_scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
